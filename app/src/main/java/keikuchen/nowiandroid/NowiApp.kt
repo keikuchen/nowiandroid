7f197db99ee259ec3dcf662862bb6c8f79022403
keikuchen <keikuchen+git@gmail.com>
2023-01-21 12:40:53 +0900

    ActivityとComposableを分離

---
+++ app/src/main/java/keikuchen/nowiandroid/NowiApp.kt
@@ -0,0 +1,36 @@
+package keikuchen.nowiandroid
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import keikuchen.nowiandroid.ui.theme.NowiAndroidTheme
+
+@Composable
+fun NowiApp() {
+    NowiAndroidTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Greeting("Android")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    NowiAndroidTheme {
+        Greeting("Android")
+    }
+}
