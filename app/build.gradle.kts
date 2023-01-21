66c3df24dfa71f315b2d188a697edd2899ef4d79
keikuchen <keikuchen+git@gmail.com>
2023-01-21 11:35:07 +0900

    ktsに移行

---
+++ app/build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    kotlin("android")
+    id("com.android.application")
+}
+
+android {
+    namespace = "keikuchen.nowiandroid"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "keikuchen.nowiandroid"
+        minSdk = 30
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.3.0"
+    }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    implementation(libs.bundles.base)
+
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.bundles.compose)
+
+    androidTestImplementation(libs.bundles.androidtest)
+    debugImplementation(libs.bundles.debug)
+    testImplementation(libs.bundles.test)
+}
---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    kotlin("android") version "1.7.10" apply false
+    id("com.android.application") version "7.4.0-beta02" apply false
+    id("com.android.library") version "7.4.0-beta02" apply false
+}
